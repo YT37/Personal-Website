db5951d9e6a727926c497d6a11bc3fa3981bd7a9
Yug Thapar <yugthapar37@gmail.com>
2025-11-20 22:10:58 -0700

    Fixed ScrollProgress Bug
    Signed-off-by: Yug Thapar <yugthapar37@gmail.com>

--- Source Code/website/src/components/ScrollProgress.tsx
+++ Source Code/website/src/components/ScrollProgress.tsx
@@ -3,19 +3,19 @@
 import { motion, useScroll, useSpring } from "framer-motion";
 
 const ScrollProgress = () => {
-    const { scrollYProgress } = useScroll();
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001
-    });
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-    return (
-        <motion.div
-            className="fixed top-0 left-0 right-0 h-1 bg-neon-primary origin-left z-[101]"
-            style={{ scaleX }}
-        />
-    );
+  return (
+    <motion.div
+      className="fixed top-0 left-0 right-0 h-1 bg-neon-primary origin-left z-[101]"
+      style={{ scaleX }}
+    />
+  );
 };
 
 export default ScrollProgress;
